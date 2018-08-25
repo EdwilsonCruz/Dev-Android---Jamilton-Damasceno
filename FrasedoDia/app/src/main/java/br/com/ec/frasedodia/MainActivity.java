71ecd554c36d327f3295845d6a7b2cb7e730c4f3
Edwilson Cruz <edwilson.cruz@al.forsoft.org.br>
2018-08-25 19:38:58 -0300

    layout e ajustes
    subindo primeira mundança do projeto FraseDoDia

--- FrasedoDia/app/src/main/java/br/com/ec/frasedodia/MainActivity.java
+++ FrasedoDia/app/src/main/java/br/com/ec/frasedodia/MainActivity.java
@@ -2,12 +2,24 @@ package br.com.ec.frasedodia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textFraseDia;
+    private Button btnFrase;
+    private String[] frases = {"testando", "Segunda frase do dia"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textFraseDia = (TextView) findViewById(R.id.textFraseDia);
+        btnFrase     = (Button) findViewById(R.id.btnFrase);
+
+        textFraseDia.setText(frases[1].toUpperCase().toString());
     }
 }
